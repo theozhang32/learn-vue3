cf1de0f40068a7ad6b1826f4c4b305a9795f9c1e
theozhang32 <theozhang.tech@outlook.com>
2022-01-22 11:38:36 +0800

    学习记录更新

--- packages/runtime-dom/src/nodeOps.ts
+++ packages/runtime-dom/src/nodeOps.ts
@@ -6,6 +6,7 @@ const doc = (typeof document !== 'undefined' ? document : null) as Document
 
 const staticTemplateCache = new Map<string, DocumentFragment>()
 
+// 原生Dom方法
 export const nodeOps: Omit<RendererOptions<Node, Element>, 'patchProp'> = {
   insert: (child, parent, anchor) => {
     parent.insertBefore(child, anchor || null)
